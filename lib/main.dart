74a9f2029667144ca537d813c43937ca22090f3f
Macbook Pro <mac@Macbooks-MacBook-Air.local>
2021-10-27 10:17:48 +0800

    animated switcher example

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Animated Container'),
+      home: const MyHomePage(title: 'Flutter Animated Switcher'),
     );
   }
 }
